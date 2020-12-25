6407951e66371416d55a0c6363e8f4e3994363db
Nick Fisher <nickfish0311@gmail.com>
2020-12-25 16:57:49 +0000

    Added resource for thread and comment

---
+++ app/Http/Resources/ThreadResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ThreadResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'content'=>$this->content,
+            'author'=>$this->author,
+            'comments'=>$this->comments,
+            'author_id'=>$this->author_id,
+        ];
+    }
+}
