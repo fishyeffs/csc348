2c47df43df18ca576befdfa9b269a4813d9f24c6
Nick Fisher <nickfish0311@gmail.com>
2020-12-28 00:51:53 +0000

    updated thread card style

--- resources/js/components/Threads.vue
+++ resources/js/components/Threads.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-        <h2 class="nav-header-text">Threads</h2>
-        <div class="card mb-6" v-for="thread in threads"
+    <div class="thread-content">
+        <h2 class="">Threads</h2>
+        <div class="card thread-container" v-for="thread in threads"
         v-bind:key="thread.id">
             <h3>{{ thread.title }}</h3>
             <p>{{ thread.content }}</p>
         </div>
+        
     </div>
 </template>
 
@@ -44,3 +45,17 @@
         }
     }
 </script>
+
+<style scoped>
+    .card {
+        max-width: 50%;
+        margin: 0 auto;
+        float: none;
+    }  
+    .thread-container {
+        padding-top: 10px;
+        padding-left: 20px;
+        padding-right: 20px;
+        background: #EEEEFF;
+    }
+</style>
